d85a5505fe42555e915ef37327741761c7019617
Kushal1825 <kushalkalsariya@gmail.com>
2025-03-02 12:47:05 +0530

    third

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -15,6 +15,8 @@ const cookieOption = {
   httpOnly: true,
   secure: process.env.NODE_ENV !== "development",
   maxAge: 7 * 24 * 60 * 60 * 1000,
+  path:"/",
+  
 };
 const cookieClear = {
   httpOnly: true,

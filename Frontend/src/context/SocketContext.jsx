92dbbbfcd0f8761aac5f8e7807980074ea350bf5
Kushal1825 <kushalkalsariya@gmail.com>
2025-03-05 21:05:02 +0530

    socket for online

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
   const { profile } = useContext(ApiContext);
   useEffect(() => {
-    const socket = io("http://localhost:5000", {
+    const socket = io("https://unitymedia.onrender.com", {
       query: {
         userId: profile?._id,
       },
